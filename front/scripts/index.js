ad46cb3b959c0b415415481472a8b56583123c59
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2025-03-17 21:10:42 -0500

    Primer Homehork del modulo2, finalizada con éxito

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,19 @@
-console.log(tempData);
+const cardsContainer = document.querySelector('.cards-container');
+
+const createCards = () => {
+    const cardsHTML = tempData.map(movie => `
+        <div class="card">
+            <img src="${movie.poster}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>Director: ${movie.director}</p>
+            <p>Duración: ${movie.duration}</p>
+            <p>Género: ${movie.genre.join(', ')}</p>
+            <p>Año: ${movie.year}</p>
+            <p>Calificación: ${movie.rate}</p>
+        </div>
+    `).join('');
+
+    cardsContainer.innerHTML = cardsHTML;
+}
+
+createCards();
