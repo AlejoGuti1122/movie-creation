42c28df93d4733c4a0b4860afcf9c160e66e0431
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2025-04-01 18:36:59 -0500

    ultimos cambios

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const app = require("./src/server")
 const dbCon = require("./src/config/dbCon")
 
 dbCon().then((res) => {
-  const PORT = 3000
+  const PORT = process.env.PORT || 3000
 
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
