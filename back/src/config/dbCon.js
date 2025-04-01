2ed28e4dd9e5966c8adcbc1044eb1f6141c73afb
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2025-04-01 11:10:43 -0500

    proyecto finalizado

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -1,11 +1,12 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 const mongoose = require("mongoose")
 
 const dbCon = async () => {
   try {
     //Realizar la conexion con la base de datos
-    await mongoose.connect(
-      "mongodb+srv://cgutierrezdalejandro:x0GRZWiit4OHYE6c@movies.udnrt1m.mongodb.net/prueba"
-    )
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("✅ Conectado a la base de datos MongoDB Atlas")
     return true
   } catch (error) {
